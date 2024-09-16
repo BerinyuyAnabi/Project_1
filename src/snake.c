bccd0d3bfd6190ce424a12daf3e546ca73ecd3b2
Logan <95838299+BerinyuyAnabi@users.noreply.github.com>
2024-09-15 22:58:27 -0700

    Update snake.c

--- src/snake.c
+++ src/snake.c
@@ -41,56 +41,54 @@ int main(int argc, char *argv[]) {
 
   /* Task 7 */
 
-  // Read board from file, or create default board
-  if (in_filename != NULL) {
-    // TODO: Load the board from in_filename
-    FILE *in_file = fopen(in_filename, "r");
-    // TODO: If the file doesn't exist, return -1
-    if (in_file == NULL) {
-      fprintf(stderr, "Error: Failed to open input file '%s'.\n", in_filename);
-      return -1;
+   // Read board from file, or create default board
+    if (in_filename != NULL) {
+        FILE *in_file = fopen(in_filename, "r");
+        if (in_file == NULL) {
+            fprintf(stderr, "Error: Failed to open input file '%s'.\n", in_filename);
+            return 1;
+        }
+        state = load_board(in_file);
+        fclose(in_file);
+        if (state == NULL) {
+            fprintf(stderr, "Error: Failed to load board from file '%s'.\n", in_filename);
+            return 1;
+        }
+    } else if (io_stdin) {
+        state = load_board(stdin);
+        if (state == NULL) {
+            fprintf(stderr, "Error: Failed to load board from stdin.\n");
+            return 1;
+        }
+    } else {
+        state = create_default_state();
+        if (state == NULL) {
+            fprintf(stderr, "Error: Failed to create default state.\n");
+            return 1;
+        }
     }
-    // TODO: Then call initialize_snakes on the state you made
-    state = load_board(in_file);
-    // TODO: close file pointer
-    fclose(in_file);
 
-    // Intitialising snakes
+    // Initialize snakes
     initialize_snakes(state);
-    
-  } else if (io_stdin) {
-    // TODO: Load the board from stdin
-    state = load_board(stdin);
-    // TODO: Then call initialize_snakes on the state you made
-    initialize_snakes(state);
-  } else {
-    // TODO: Create default state
-    state = create_default_state();
-  }
 
-  // TODO: Update state. Use the deterministic_food function
-  // (already implemented in snake_utils.h) to add food.
-  deterministic_food(state);
+    // Update state. Use the deterministic_food function to add food.
+    deterministic_food(state);
 
-  // Write updated board to file or stdout
-  if (out_filename != NULL) {
-    // TODO: Save the board to out_filename
-    FILE *out_file = fopen(out_filename, "w");
-    if (out_file == NULL) {
-      fprintf(stderr, "Error: Failed to open output file '%s'.\n", out_filename);
-      free_state(state);
-      return -1;
+    // Write updated board to file or stdout
+    if (out_filename != NULL) {
+        FILE *out_file = fopen(out_filename, "w");
+        if (out_file == NULL) {
+            fprintf(stderr, "Error: Failed to open output file '%s'.\n", out_filename);
+            free_state(state);
+            return 1;
+        }
+        print_board(state, out_file);
+        fclose(out_file);
+    } else {
+        print_board(state, stdout);
     }
-    // Save board to the file
-    print_board(state, out_file);
-    fclose(out_file);
-    return -1;
-  } else {
-    // TODO: Print the board to stdout
-    print_board(state, stdout);
-  }
 
-  // TODO: Free the state
-  free_state(state);
-  return 0;
+    // Free the state
+    free_state(state);
+    return 0;
 }
